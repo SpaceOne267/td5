778c1bb437e89d3ac0bfa619c06722b2b544c8c0
SpaceOne267 <140384854+SpaceOne267@users.noreply.github.com>
2024-08-26 17:21:38 +0500

    Add files via upload

---
+++ project-app/src/sections/games.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+// components
+import GamesPlayCards from "../components/games.play.cards";
+import GamesRooms from "../components/games.rooms";
+import Preloader from "../includes/preloader";
+// NavBar
+import NavBar from "../components/nav.bar";
+
+const Games = () => {
+  return (
+    <section className="page menu_section pb-80">
+      <Preloader />
+      <div className="container">
+        {/* play cards */}
+        <GamesPlayCards />
+        {/* rooms */}
+        <GamesRooms />
+        {/* nav */}
+        <NavBar />
+      </div>
+    </section>
+  );
+};
+export default Games;
